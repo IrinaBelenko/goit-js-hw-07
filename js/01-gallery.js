a3ed9ca4b45824c9583a11b52423a4d43c4fd1ed
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2023-04-06 00:42:14 +0300

    01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryMarkup = createGalleryMarkup(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
 
-console.log(galleryItems);
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+    </li>
+    `;
+    })
+    .join("");
+}
+
+galleryRef.addEventListener("click", onGalleryRefClick);
+
+function onGalleryRefClick(e) {
+  e.preventDefault();
+  //const galleryImage =
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const sourceImg = e.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${sourceImg}">
+`);
+
+  instance.show();
+}
